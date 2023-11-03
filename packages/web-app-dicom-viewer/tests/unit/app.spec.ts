7ec3b3e9073239e496a22039f4a35af87811ea19
Kathrin Koebel <kathrin@koebel.ch>
2023-11-03 13:28:59 +0545

    adding test for format label method

--- packages/web-app-dicom-viewer/tests/unit/app.spec.ts
+++ packages/web-app-dicom-viewer/tests/unit/app.spec.ts
@@ -1,3 +1,4 @@
+import { Resource } from 'web-client/src'
 import App from '../../src/App.vue'
 
 import { nextTick, ref } from 'vue'
@@ -15,11 +16,13 @@ import { mock } from 'jest-mock-extended'
 
 // -------------------------------------------------
 // suggested test cases
+
 // - is Cornerstone core initalized on "mounted" in vue lifecyle
 // - is RenderEngine enabled (on "mounted")
 // - is div element with id="dicom-canvas" visible (on "mounted")
 // - does canvas element with class="cornerstone-canvas" exist? (on "mounted")
 // - test addWadouriPrefix() function (done)
+// - test more custom functions like fetch vip metadata, fetch metadata, etc.
 // - when a dcm file (mock or real file? --> upload local file) is set on stack, does the viewport then contain the corresponding data (by getting the content of the file as datastring)?
 // - when a dcm file is set on stack, does the viewport then contain the corresponding basic meta data, e.g. user name (also UI still needs to be implemented, see screenshots)?
 // - maybe test if the correct sop class is displayed (will only be visible in show metadata, not yet implemented)
@@ -36,7 +39,7 @@ import { mock } from 'jest-mock-extended'
 // implementation of test cases
 // -------------------------------------------------
 
-// defining data
+// defining data (not sure if this is needed)
 const dicomFiles = [
   {
     id: '1',
@@ -49,8 +52,8 @@ const dicomFiles = [
 const dicomTestFilePath = './testfiles/MRBRAIN.dcm' // check if this needs to be an import or a const
 
 // test cases
-// dummy test case
-describe('Dicom viewer app', () => {
+// dummy test case, for testing only (to be deleted)
+describe('dicom viewer app', () => {
   describe('dummy test', () => {
     it('do nothing :)', () => {
       expect(dicomTestFilePath).toBe(dicomTestFilePath)
@@ -59,13 +62,29 @@ describe('Dicom viewer app', () => {
 })
 
 // test addWadouriPrefix() method
-describe('Dicom viewer app', () => {
+describe('dicom viewer app', () => {
   describe('Method "addWadouriPrefix"', () => {
     it('should add wadouri prefix to dicom file path', async () => {
       const dicomURL = 'https://dav/spaces/path/to/file.dcm?OC-Credential=xyz'
       const wadouriDicomURL = 'wadouri:https://dav/spaces/path/to/file.dcm?OC-Credential=xyz'
       const modifiedURL = await App.methods.addWadouriPrefix(dicomURL)
       expect(modifiedURL).toEqual(wadouriDicomURL)
+    })
+  })
+})
+
+// test formatLabel() method
+describe('dicom viewer app', () => {
+  describe('Method "formatLabel()"', () => {
+    it('should format a metadata variable name into a nicely readible label', () => {
+      const label = 'patientName'
+      const formatedLabel = 'Patient Name'
+      expect(App.methods.formatLabel(label)).toEqual(formatedLabel)
+    })
+    it('should format a metadata variable name with underlines into a nicely readible label', () => {
+      const label = 'SOP_InstanceUID'
+      const formatedLabel = 'SOP Instance UID'
+      expect(App.methods.formatLabel(label)).toEqual(formatedLabel)
     })
   })
 })
